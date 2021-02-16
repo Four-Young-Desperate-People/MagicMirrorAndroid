23890b86ffb53d0be4ba119f6c3a9891a3844552
Tim Zheng <tim.q.zheng@gmail.com>
2021-02-15 19:51:05 -0500

    Amending some git tomfoolery

--- app/src/main/java/com/example/heartratealarm/EditAlarmActivity.java
+++ app/src/main/java/com/example/heartratealarm/EditAlarmActivity.java
@@ -1,14 +1,23 @@
 package com.example.heartratealarm;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+
 public class EditAlarmActivity extends AppCompatActivity {
+    public static final int REQ_PICK_AUDIO = 10001;
+    private static final String TAG = "EditAlarmActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +28,29 @@ public class EditAlarmActivity extends AppCompatActivity {
         btnRingtone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-                startActivityForResult(intent, 1);
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQ_PICK_AUDIO);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != REQ_PICK_AUDIO || resultCode != RESULT_OK || data == null){
+            return;
+        }
+        Uri uri = data.getData();
+        String songPath = uri.getPath();
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(songPath);
+        } catch (IOException e) {
+            Log.e(TAG, "onActivityResult: " + e.getMessage());
+        }
+        mediaPlayer.start();
+
     }
 }
