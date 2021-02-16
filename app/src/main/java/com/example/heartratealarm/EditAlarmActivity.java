df9f64a6f6551396ea0386d4bef423021f8a1a6e
DomFaryna <dominic.faryna@gmail.com>
2021-02-15 21:54:44 -0500

    We now have music baby

--- app/src/main/java/com/example/heartratealarm/EditAlarmActivity.java
+++ app/src/main/java/com/example/heartratealarm/EditAlarmActivity.java
@@ -1,10 +1,17 @@
 package com.example.heartratealarm;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +19,9 @@ import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.loader.content.CursorLoader;
 
 import java.io.IOException;
 
@@ -22,6 +32,10 @@ public class EditAlarmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String[] perms = {Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (ContextCompat.checkSelfPermission(this, perms[0]) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, perms, 123);
+        }
         setContentView(R.layout.activity_edit_alarm);
         Button btnRingtone = (Button) this.findViewById(R.id.selectRingtone);
 
@@ -29,8 +43,9 @@ public class EditAlarmActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_PICK,
-                            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, REQ_PICK_AUDIO);
+                        android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(intent, 1234);
             }
         });
     }
@@ -38,19 +53,29 @@ public class EditAlarmActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode != REQ_PICK_AUDIO || resultCode != RESULT_OK || data == null){
+        if (resultCode != RESULT_OK || requestCode != 1234 || data == null){
             return;
         }
         Uri uri = data.getData();
         String songPath = uri.getPath();
 
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        try {
-            mediaPlayer.setDataSource(songPath);
-        } catch (IOException e) {
-            Log.e(TAG, "onActivityResult: " + e.getMessage());
-        }
-        mediaPlayer.start();
+        String path = null;
+        String[] projection = { MediaStore.Audio.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        //Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
 
+        if(cursor == null){
+            path = uri.getPath();
+        }
+        else{
+            cursor.moveToFirst();
+            int column_index = cursor.getColumnIndexOrThrow(projection[0]);
+            path = cursor.getString(column_index);
+            cursor.close();
+        }
+
+        Uri GoodUri = Uri.parse("file://" + path);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, GoodUri);
+        mediaPlayer.start();
     }
 }
