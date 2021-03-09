159ab1da3cb3b827bd03080ac44bf0faa74e1a61
DomFaryna <dominic.faryna@gmail.com>
2021-03-09 17:15:49 -0500

    Happy path done

--- app/src/main/java/com/example/heartratealarm/alarm/Alarm.java
+++ app/src/main/java/com/example/heartratealarm/alarm/Alarm.java
@@ -5,6 +5,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -16,6 +17,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
@@ -23,10 +25,17 @@ import androidx.room.PrimaryKey;
 import com.example.heartratealarm.AlarmReceiver;
 import com.example.heartratealarm.MainActivity;
 import com.example.heartratealarm.R;
+import com.example.heartratealarm.websocket.GenericData;
+import com.example.heartratealarm.websocket.WebSocketBase;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -110,7 +119,7 @@ public class Alarm {
             alarmMp.setScreenOnWhilePlaying(true);
             MediaPlayer exerciseMp;
             Uri exerciseSong;
-            boolean hasExerciseSong = false;
+            boolean hasExerciseSong;
             if (runningAlarm.exercisePath != null) {
                 exerciseSong = Uri.parse(runningAlarm.exercisePath);
                 exerciseMp = MediaPlayer.create(context, exerciseSong);
@@ -118,43 +127,115 @@ public class Alarm {
                 exerciseMp.setLooping(true);
                 exerciseMp.setScreenOnWhilePlaying(true);
                 hasExerciseSong = true;
+                // This else looks retarted, but is needed to make these variables final lambda safe
+            } else {
+                hasExerciseSong = false;
+                exerciseMp = null;
             }
 
             // Play Music
             Log.d(TAG, "runAlarm: playing music");
             alarmMp.start();
 
-            // TODO: Connect to Mirror
-            AlarmJson json = new AlarmJson(runningAlarm.exercise, runningAlarm.brightness);
-
-
-            // TODO: Query and get information on exercising queue
-
             // Make a window, TODO: test with lock screen
-            WindowManager.LayoutParams p = new WindowManager.LayoutParams(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
+            WindowManager.LayoutParams
+                    p = new WindowManager.LayoutParams(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
             WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View myView = inflater.inflate(R.layout.activity_alarm, null, false);
             Button btnDismiss = myView.findViewById(R.id.alarmDismissButton);
-            // TODO: maybe change button color to be greyed out?
-            windowManager.addView(myView, p);
-            // TODO: add logic for disabling dismiss Button
-            btnDismiss.setOnClickListener(v -> {
-                if (false) {
-                    Toast.makeText(context, "Go To Mirror and Exercise!", Toast.LENGTH_SHORT).show();
-                }else{
+
+            WebSocketBase ws = new WebSocketBase();
+            Gson gson = new Gson();
+            AtomicBoolean pinged = new AtomicBoolean(false);
+            AtomicBoolean allowDismiss = new AtomicBoolean(false);
+            AtomicInteger missedCount = new AtomicInteger(0);
+
+            Runnable stop = () -> {
+                if (alarmMp.isPlaying()) {
                     alarmMp.stop();
-                    windowManager.removeView(myView);
-                    exitAlarm(context);
+                }
+                if (hasExerciseSong && exerciseMp.isPlaying()) {
+                    exerciseMp.stop();
+                }
+                windowManager.removeView(myView);
+                exitAlarm(context);
+                ws.close();
+            };
+
+            Disposable iveStoppedGivingAFuck = ws.getMessageObservable().subscribe(s -> {
+                GenericData gd = gson.fromJson(s, GenericData.class);
+                if (gd.method.equals("pong")) {
+                    missedCount.set(0);
+                    pinged.set(false);
+                    Log.i(TAG, "ponged");
+                } else if (gd.method.equals("quiet_alarm")) {
+                    Log.d(TAG, "Got a quiet alarm");
+                    Type type = new TypeToken<GenericData<Boolean>>() {
+                    }.getType();
+                    GenericData<Boolean> shouldWeQuite = gson.fromJson(s, type);
+                    if (shouldWeQuite.data) {
+                        alarmMp.stop();
+                        if (hasExerciseSong) {
+                            exerciseMp.start();
+                        }
+                    } else {
+                        if (hasExerciseSong) {
+                            exerciseMp.stop();
+                        }
+                        alarmMp.start();
+                    }
+                } else if (gd.method.equals("stop_alarm")) {
+                    Log.d(TAG, "Got a stop alarm");
+                    stop.run();
+                }
+            }, e -> {
+                Log.e(TAG, "Got an err from system websocket", e);
+                // duplicate code... sue me
+                allowDismiss.set(true);
+                btnDismiss.setBackgroundTintList(ContextCompat.getColorStateList(context, R.color.purple_500));
+            }, () -> {
+                Log.i(TAG, "Completed recieving from websocket completed");
+            });
+
+            ws.interval(s -> {
+                GenericData<Long> gd = new GenericData<>("ping", s);
+                if (pinged.get()) {
+                    int missed = missedCount.incrementAndGet();
+                    Log.i(TAG, String.format("missed ping, current count %d", missed));
+
+                    // we have missed 3 pings. Sever is ignore us or is down. Enable the button.
+                    if (missed > 5) {
+                        allowDismiss.set(true);
+                        btnDismiss.setBackgroundTintList(ContextCompat.getColorStateList(context, R.color.purple_500));
+                    }
+                }
+                pinged.set(true);
+                ws.send(gson.toJson(gd));
+            });
+
+            // start this whole shebang
+            AlarmJson json = new AlarmJson(runningAlarm.exercise, runningAlarm.brightness);
+            GenericData<AlarmJson> gd = new GenericData("alarm_start", json);
+            ws.send(gson.toJson(gd));
+
+            windowManager.addView(myView, p);
+
+            // TODO define as a custom colour in colors.xml. Dom doesn't really care about it rn.
+            btnDismiss.setBackgroundTintList(ColorStateList.valueOf(Color.GRAY));
+            btnDismiss.setOnClickListener(v -> {
+                if (allowDismiss.get()) {
+                    stop.run();
+                } else {
+                    Toast.makeText(context, "Go To Mirror and Exercise!", Toast.LENGTH_SHORT).show();
                 }
             });
 
         }, e -> {
             Log.e(TAG, "Alarm Load: ", e);
         });
-
-
     }
+
 
     static public void exitAlarm(Context context) {
         Log.d(TAG, "Alarm: Exiting alarm");
